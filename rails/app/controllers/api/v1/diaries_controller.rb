b716eabbc08086d235d56e408703b5efc15fda2f
Haruto9981 <aketaharuto46@icloud.com>
2024-04-21 15:16:42 +0900

    add diary index and show

---
+++ rails/app/controllers/api/v1/diaries_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::DiariesController < Api::V1::BaseController
+  include Pagination
+
+  def index
+    diaries = Diary.shared.order(created_at: :desc).page(params[:page] || 1).per(10).includes(:user)
+    render json: diaries, meta: pagination(diaries), adapter: :json
+  end
+
+  def show
+    diary = Diary.shared.find(params[:id])
+    render json: diary
+  end
+end
--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+  user1 = User.create!(name: "テスト太郎", email: "test1@example.com", password: "password", confirmed_at: Time.current)
+
+  user2 = User.create!(name: "テスト次郎", email: "test2@example.com", password: "password", confirmed_at: Time.current)
+
+  15.times do |i|
+    Diary.create!(title: "テストタイトル1-#{i}", content: "テスト本文1-#{i}", word_count: 100, status: :shared, user: user1)
+    Diary.create!(title: "テストタイトル2-#{i}", content: "テスト本文2-#{i}", word_count: 100, status: :shared, user: user2)
+  end
+end
